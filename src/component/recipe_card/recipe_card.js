e782a682c6297d1f6911aff92f2050c5886c4779
abbassu <abbassurakji99@gmail.com>
2023-01-16 23:26:26 +0300

    add photo

--- src/component/recipe_card/recipe_card.js
+++ src/component/recipe_card/recipe_card.js
@@ -3,17 +3,19 @@ import "./recipe_card.css"
 import { Link } from "react-router-dom";
 function Recipe_card ({recipe}){
     return(
-
-        <Link className="card"  to="/recipe" state={recipe}>
+<div>
+<Link className="card"  to="/recipe" state={recipe}>
             <p className="title">{recipe.title}</p>
             {   recipe.image ?
                     <img src={recipe.image } alt="" className="responsive" />
                     : 
-                    <h2 className="no_photo"> THERE IS NO PHOTO </h2>
+                    <h2 className="no_photo"> -THERE IS NO PHOTO-  </h2>
             }
             
         </Link>
         
+</div>
+
     )
 }
 export default Recipe_card
